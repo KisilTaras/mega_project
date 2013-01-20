97eaefa88e7aabbf1eb64558cc8a80fb70083fa4
ruby -v <kisilto@ukr.net>
2013-01-20 22:40:18 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MegaProject::Application.config.secret_token = 'af317d48d5aeca956d57a1e408418ad0bb0e640e4eac97cb05858205278b0c1b2789280ade1da75656f269c81e1f74a0926fc8b01575f7a6940da2c6c786a6b4'
